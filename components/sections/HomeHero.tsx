c794a5ca7e861510a40411d26959f3bb2a1168b2
osemensilas <osemensilas@gmail.com>
2026-04-18 13:52:39 +0100

    update

--- components/sections/HomeHero.tsx
+++ components/sections/HomeHero.tsx
@@ -4,7 +4,7 @@ import ContactBtn from '@/components/ui/ContactBtn';
 const HomeHero = () => {
   return (
     <>
-      <section className="relative h-[calc(100vh-50px)] w-screen flex items-center justify-start">
+      <section className="relative h-max w-screen py-40 px-10 flex items-center justify-start">
         <div className="absolute h-full w-full">
           <Image src="/" fill className="object-cover" alt="hero image" />
         </div>
--- components/sections/WhySection.tsx
+++ components/sections/WhySection.tsx
@@ -1,7 +1,43 @@
 const WhySection = () => {
   return(
     <>
-      
+      <section className="h-max w-full px-10 py-20">
+        <h2 className="text-text text-3xl font-bold text-center">Why Choose Us</h2>
+        <div className="h-max w-full mt-20 flex space-evenly">
+          <div className="h-max w-[200px] bg-background rounded shadow">
+            <div className="h-max w-full mb-5">
+              <div className="relative h-20 w-20">
+                <Image src="/" fill className="object-cover" />
+              </div>
+            </div>
+            <p className="text-text text-base text-center">Fast response time</p>
+          </div>
+          <div className="h-max w-[200px] bg-background rounded shadow">
+            <div className="h-max w-full mb-5">
+              <div className="relative h-20 w-20">
+                <Image src="/" fill className="object-cover" />
+              </div>
+            </div>
+            <p className="text-text text-base text-center">Fast response time</p>
+          </div>
+          <div className="h-max w-[200px] bg-background rounded shadow">
+            <div className="h-max w-full mb-5">
+              <div className="relative h-20 w-20">
+                <Image src="/" fill className="object-cover" />
+              </div>
+            </div>
+            <p className="text-text text-base text-center">Fast response time</p>
+          </div>
+          <div className="h-max w-[200px] bg-background rounded shadow">
+            <div className="h-max w-full mb-5">
+              <div className="relative h-20 w-20">
+                <Image src="/" fill className="object-cover" />
+              </div>
+            </div>
+            <p className="text-text text-base text-center">Fast response time</p>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
