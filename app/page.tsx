8e755144adca6a2e411b9f17d3df31001d49a5be
osemensilas <osemensilas@gmail.com>
2026-04-18 10:50:51 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 const Page = () => {
-  return (
+  return ( 
     <>
-     <section className="h-max w-screen px-10 py-40">
-       
-     </section>
+    
     </>
-  )
+   );
 }
-
+ 
 export default Page;
