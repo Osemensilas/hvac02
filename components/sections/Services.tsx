37498e38e5fe19c152a25fcb8606316f561793a9
osemensilas <osemensilas@gmail.com>
2026-04-18 17:25:55 +0100

    update Services.tsx

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 const Services = () => {
   return(
     <>
-      <section className="h-max w-screen px-10 py-40">
+      <section className="h-max w-screen px-10 py-20">
         <h2 className="text-text text-3xl font-bold text-center">Our Services</h2>
-        <div className="h-max w-full mt-20 flex items-center justify-center gap-5">
+        <div className="h-max w-full mt-20 flex flex-wrap items-center justify-center gap-5">
           <div className="h-max w-[250px] bg-background rounded shadow p-2">
             <div className="h-max w-full mb-5 items-center justify-center">
               <div className="relative h-20 w-20">
