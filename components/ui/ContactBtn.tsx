1279142de816f1bd33b85c2e718ed538a2f2f965
osemensilas <osemensilas@gmail.com>
2026-04-20 12:34:59 +0100

    update ContactBtn.tsx

--- components/ui/ContactBtn.tsx
+++ components/ui/ContactBtn.tsx
@@ -1,8 +1,8 @@
 const ContactBtn = () => {
     return ( 
         <>
-        <a href="/contact" className="bg-tranparent border border-primary text-primary py-2 px-4 rounded">
-            +234 704 7880 486 Contact Us
+        <a href="tel:+2347047880486" className="bg-tranparent border border-primary text-primary py-2 px-4 rounded">
+            Call Us
         </a>
         </>
      );
