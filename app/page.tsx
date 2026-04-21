286d529c0dbc886969b649936b3d3f48e550e5d4
osemensilas <osemensilas@gmail.com>
2026-04-21 08:25:34 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const Page = () => {
     <FAQs />
     <ContactUs />
     <section className="h-max w-screen bg-header py-16 px-6 flex flex-col items-center gap-6">
-      <h3 className="text-2xl text-accent font-bold">Schedule an appointment with us for your HVAC needs today</h3>
+      <h3 className="text-2xl text-accent font-bold text-center">Schedule an appointment with us for your HVAC needs today</h3>
       <ScheduleBtn />
     </section>
     </>
